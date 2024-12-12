61ef9e5b973f9e5569c90390a739be5c8ca2b060
Thawan Fidelis <thawan.fidelis@gmail.com>
2024-12-12 11:55:47 -0300

    Add lambda permission

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -4,7 +4,6 @@ data "archive_file" "zip" {
   output_path = "../lambda/hello.zip"
 }
 
-#-> https://github.dev/felipebarras/lambda-authentication/blob/main/.github/workflows/deploy-lambda.yaml
 resource "aws_lambda_function" "hello_thawan" {
   filename         = data.archive_file.zip.output_path
   function_name    = "node_js_lambda_authorizer"
@@ -12,6 +11,7 @@ resource "aws_lambda_function" "hello_thawan" {
   handler          = "hello_thawan.handler"
   runtime          = "nodejs18.x"
 
+  # TODO: add to get users from RDS
   # environment {
   #   variables = {
   #     eks_find_by_cpf_endpoint = var.eks_find_by_cpf_endpoint
@@ -19,4 +19,12 @@ resource "aws_lambda_function" "hello_thawan" {
   # }
 
   source_code_hash = data.archive_file.zip.output_base64sha256
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_thawan.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
 }
