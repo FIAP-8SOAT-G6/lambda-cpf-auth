288cc6f3f561e6bf69f8db3f25dc01315fe79252
Thawan Fidelis <thawan.fidelis@gmail.com>
2024-12-14 08:59:35 -0300

    update aws_apigateway to v2

--- infra/api-gateway.tf
+++ infra/api-gateway.tf
@@ -1,73 +1,23 @@
-resource "aws_api_gateway_rest_api" "api_gateway" {
-  name = "nosso-api-gateway" # TODO - Change this name
-  description = "API Gateway criado com terraform"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
+resource "aws_apigatewayv2_api" "api_gateway" {
+  name          = "tech-challenge-api-gateway"
+  protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part = "hello"
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                  = aws_apigatewayv2_api.api_gateway.id
+  integration_type        = "AWS_PROXY"
+  integration_uri         = aws_lambda_function.hello.invoke_arn
+  payload_format_version  = "2.0"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_resource.root.id
-  http_method   = "GET"
-  authorization = "NONE"
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "GET /lambda"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
-resource "aws_api_gateway_method_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = "200"
-
-  //cors
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
-
-  integration_http_method = "GET"
-  type = "AWS_PROXY"
-  uri = aws_lambda_function.hello.invoke_arn
-}
-
-resource "aws_api_gateway_integration_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = aws_api_gateway_method_response.proxy.status_code
-
-   //cors
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  depends_on = [
-    aws_api_gateway_method.proxy,
-    aws_api_gateway_integration.lambda_integration
-  ]
-}
-
-resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name = "dev"
-
-  depends_on = [
-    aws_api_gateway_integration.lambda_integration
-  ]
+resource "aws_apigatewayv2_stage" "dev_stage" {
+  api_id      = aws_apigatewayv2_api.api_gateway.id
+  name        = "dev"
+  auto_deploy = true
 }
